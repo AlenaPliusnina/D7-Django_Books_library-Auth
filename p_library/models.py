01f59a91451cd9189ee50bbc73e6a359c792f0f2
AlenaPliusnina <a564352@gmail.com>
2020-10-14 14:12:28 +0300

    update age type UserProfile

--- p_library/models.py
+++ p_library/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 
 class UserProfile(models.Model):
-    age = models.IntegerField(null=True)
+    age = models.PositiveIntegerField(null=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
 
 
